f86232c404d06b3391d47982db9a70862d2bf2d1
ZhouXY108 <luquanlion@outlook.com>
2025-06-08 13:09:44 +0800

    docs: 修改 javadoc 中的格式错误

--- plusone-validator/src/main/java/xyz/zhouxy/plusone/validator/ComparablePropertyValidator.java
+++ plusone-validator/src/main/java/xyz/zhouxy/plusone/validator/ComparablePropertyValidator.java
@@ -23,7 +23,6 @@ import java.util.function.Function;
  *
  * @param <T> 待校验对象的类型
  * @param <TProperty> 待校验属性的类型，必须实现 {@code Comparable} 接口
- * @param <TPropertyValidator> 具体校验器类型，用于支持链式调用
  * @see com.google.common.collect.Range
  * @author ZhouXY
  */
